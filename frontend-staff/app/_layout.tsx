3893e75e657b991b1106356bc38e05c1350a2a48
Tri <tranthanhtri23022005@gmail.com>
2026-08-14 21:56:00 +0700

    update(frontend-staff): add safe area for bottom tab

--- frontend-staff/app/_layout.tsx
+++ frontend-staff/app/_layout.tsx
@@ -6,20 +6,23 @@
 
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { QueryProvider } from "../src/providers/QueryProvider";
 
 export default function RootLayout() {
   return (
-    <QueryProvider>
-      <StatusBar style="light" />
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(app)" options={{ headerShown: false }} />
-      </Stack>
-    </QueryProvider>
+    <SafeAreaProvider>
+      <QueryProvider>
+        <StatusBar style="light" />
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(app)" options={{ headerShown: false }} />
+        </Stack>
+      </QueryProvider>
+    </SafeAreaProvider>
   );
 }
